546d5c57022bd743d6526205f24f642d5b0c47ac
kekehurry <arhukai1111@gmail.com>
2025-01-02 12:03:05 +0800

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,6 @@ const myProjects = [
 		linkText: "View Project",
 		link: "https://cityflow.cn",
 	},
-
 	{
 		title: "Graph RAG as Human Choice Model",
 		description:
@@ -16,7 +15,6 @@ const myProjects = [
 		linkText: "View Project",
 		link: "https://www.media.mit.edu/projects/graph-rag-as-human-choice-model-building-a-data-driven-simulation-framework-with-llm-agents/overview/",
 	},
-
 	{
 		title: "Human-AI Collaborative Planning",
 		description:
@@ -31,7 +29,7 @@ const myProjects = [
 			"TravelAgent simulates human-like behaviors and experiences in diverse built environments",
 		logo: "/projects/ta-scheme.png",
 		linkText: "View Project",
-		link: "https://www.arielnoyman.com/",
+		link: "https://www.arielnoyman.com/travelAgent/",
 	},
 	{
 		title: "Evolable Case-based Design",
@@ -39,6 +37,22 @@ const myProjects = [
 		logo: "/projects/evolvable-case-based-design.jpg",
 		linkText: "View Project",
 		link: "https://github.com/kekehurry/Evolvable-Case-based-Design",
+	},
+	{
+		title: "Interactive Simulation Platform",
+		description:
+			"A tangible and digital interface, inspired by MIT CityScope Project, evaluating thermal and accessibility performance in real time.",
+		logo: "/projects/interactive_simulation.jpg",
+		linkText: "View Project",
+		link: "https://mp.weixin.qq.com/s/jZWe0eDtDax3sOB3hvgdpg",
+	},
+	{
+		title: "Agent-based Model for SCUT Pedestrian System",
+		description:
+			"An ABM model simulating pedestrian flow in SCUT based on GAMA platform",
+		logo: "/projects/heatmap.png",
+		linkText: "View Project",
+		link: "https://mp.weixin.qq.com/s/si3h6txNLMHegJ92n0gq6g",
 	},
 ];
 
